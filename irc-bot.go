efcbd59eb7ed6aa6f41203bcc83f6106e8cb2deb
Anthony James Dempsey <anthonyjdempsey@gmail.com>
2016-05-21 18:55:43 +0100

    Enhance/Added code to read config file
    Added code to include a TOML config file in $HOME/.ircbot.config.
    Used library found at gihub.com/BurntSushi/toml to parse the file.
    Format of the file is
    Room_name
    Bot_name
    Password
    Server_url

--- irc-bot.go
+++ irc-bot.go
@@ -2,39 +2,60 @@ package main
 
 import (
     "github.com/thoj/go-ircevent"
+    "github.com/BurntSushi/toml"
     "fmt"
+    "os"
+    "os/user"
+    "log"
 )
 
-type config struct {
-    room_name string
-    bot_name string
-    password string
-    server_url string
+type Config struct {
+    Room_name string
+    Bot_name string
+    Password string
+    Server_url string
+}
+
+/* This code is based on this stackoverflow answer
+http://stackoverflow.com/questions/8757389/reading-file-line-by-line-in-go
+*/
+func read_config_file (file_path string) *Config {
+    var instance_config Config
+   
+    _, err := os.Stat(file_path)
+    if err != nil {
+	log.Fatal("Config file doesn't exist")
+    }
+    fmt.Println("Attempting to read config file ", file_path)
+    if _, err := toml.DecodeFile(file_path, &instance_config); err != nil {
+	log.Fatal(err)
+    }
+    fmt.Println("Config file read succesfully")
+    return &instance_config
 }
 
 
 func main() {
-    var instance_config config
-    instance_config.room_name = ""
-    instance_config.bot_name = ""
-    instance_config.password = ""
-    instance_config.server_url = ""
 
-    conn := irc.IRC(instance_config.bot_name, instance_config.bot_name)
-    conn.Password = instance_config.password
-    err := conn.Connect(instance_config.server_url)
+    usr,_ := user.Current()
+    dir :=usr.HomeDir
+    instance_config := read_config_file(dir+"/.ircbot.config")
+
+    conn := irc.IRC(instance_config.Bot_name, instance_config.Bot_name)
+    conn.Password = instance_config.Password
+    err := conn.Connect(instance_config.Server_url)
     if err != nil {
         fmt.Println("Failed to connect")
         return
     }
     conn.AddCallback("001", func (e *irc.Event) {
-        conn.Join(instance_config.room_name)
+        conn.Join(instance_config.Room_name)
     })
     conn.AddCallback("JOIN", func (e *irc.Event) {
-        conn.Privmsg(instance_config.room_name, "Hello! I am a Gobot")
+        conn.Privmsg(instance_config.Room_name, "Hello! I am a Gobot")
     })
     conn.AddCallback("PRIVMSG", func (e *irc.Event) {
-        conn.Privmsg(instance_config.room_name, e.Message())
+        conn.Privmsg(instance_config.Room_name, e.Message())
     })
     conn.Loop()
 }
