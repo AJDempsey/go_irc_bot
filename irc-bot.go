0abfc928a7a0f8f7b71b2f707437e0f5102cff3a
Anthony James Dempsey <anthonyjdempsey@gmail.com>
2016-06-13 21:42:32 +0100

    Ran gofmt over the code

--- irc-bot.go
+++ irc-bot.go
@@ -1,69 +1,67 @@
 package main
 
 import (
-    "github.com/thoj/go-ircevent"
-    "github.com/BurntSushi/toml"
-    "fmt"
-    "os"
-    "os/user"
-    "log"
-    "flag"
+	"flag"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/thoj/go-ircevent"
+	"log"
+	"os"
+	"os/user"
 )
 
 type Config struct {
-    Room_name string
-    Bot_name string
-    Password string
-    Server_url string
+	Room_name  string
+	Bot_name   string
+	Password   string
+	Server_url string
 }
 
 /* Idea for this code taken from https://blog.gopheracademy.com/advent-2014/reading-config-files-the-go-way/ */
-func read_config_file (file_path string) (*Config, bool) {
-    var instance_config Config
+func read_config_file(file_path string) (*Config, bool) {
+	var instance_config Config
 
-    _, err := os.Stat(file_path)
-    if err != nil {
-	return &instance_config, false
-    }
-    fmt.Println("Attempting to read config file ", file_path)
-    if _, err := toml.DecodeFile(file_path, &instance_config); err != nil {
-	log.Fatal(err)
-    }
-    fmt.Println("Config file read succesfully")
-    return &instance_config, true
+	_, err := os.Stat(file_path)
+	if err != nil {
+		return &instance_config, false
+	}
+	fmt.Println("Attempting to read config file ", file_path)
+	if _, err := toml.DecodeFile(file_path, &instance_config); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Config file read succesfully")
+	return &instance_config, true
 }
-
 
 func main() {
 
-    usr,_ := user.Current()
-    dir :=usr.HomeDir
+	usr, _ := user.Current()
+	dir := usr.HomeDir
 
-    config_file := flag.String("config-file", dir+"/.ircbot.config", "Path to a non-default config file")
+	config_file := flag.String("config-file", dir+"/.ircbot.config", "Path to a non-default config file")
 
-    flag.Parse()
+	flag.Parse()
 
+	instance_config, ok := read_config_file(*config_file)
 
-    instance_config, ok := read_config_file(*config_file)
-
-    if !ok {
-	log.Fatal("Config file doesn't exist, fail right now")
-    }
-    conn := irc.IRC(instance_config.Bot_name, instance_config.Bot_name)
-    conn.Password = instance_config.Password
-    err := conn.Connect(instance_config.Server_url)
-    if err != nil {
-        fmt.Println("Failed to connect")
-        return
-    }
-    conn.AddCallback("001", func (e *irc.Event) {
-        conn.Join(instance_config.Room_name)
-    })
-    conn.AddCallback("JOIN", func (e *irc.Event) {
-        conn.Privmsg(instance_config.Room_name, "Hello! I am a Gobot")
-    })
-    conn.AddCallback("PRIVMSG", func (e *irc.Event) {
-        conn.Privmsg(instance_config.Room_name, e.Message())
-    })
-    conn.Loop()
+	if !ok {
+		log.Fatal("Config file doesn't exist, fail right now")
+	}
+	conn := irc.IRC(instance_config.Bot_name, instance_config.Bot_name)
+	conn.Password = instance_config.Password
+	err := conn.Connect(instance_config.Server_url)
+	if err != nil {
+		fmt.Println("Failed to connect")
+		return
+	}
+	conn.AddCallback("001", func(e *irc.Event) {
+		conn.Join(instance_config.Room_name)
+	})
+	conn.AddCallback("JOIN", func(e *irc.Event) {
+		conn.Privmsg(instance_config.Room_name, "Hello! I am a Gobot")
+	})
+	conn.AddCallback("PRIVMSG", func(e *irc.Event) {
+		conn.Privmsg(instance_config.Room_name, e.Message())
+	})
+	conn.Loop()
 }
